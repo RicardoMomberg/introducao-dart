4dfdbcc345e4a263bf6e03ac9e04f5017c8c6a42
RicardoMomberg <65039145+RicardoMomberg@users.noreply.github.com>
2025-09-02 11:03:23 -0300

    09 - Packages and plugins Dart - package

--- introducao-dart/bin/introducao_dart.dart
+++ introducao-dart/bin/introducao_dart.dart
@@ -5,13 +5,11 @@ import 'package:introducao_dart/aula2/pedido.dart';
 import 'package:introducao_dart/aula2/produto.dart';
 import 'package:introducao_dart/aula2/sistema_loja.dart';
 import 'package:introducao_dart/logic.dart' as introducao_dart;
+import 'package:my_simple_package/my_simple_package.dart';
 
 void main(List<String> arguments) {
   print("inicializando a aplicação...");
 
-  var now = DateTime.now();
-  var formatador = DateFormat('dd/MM/yyyy hh:mm aaa');
-  String dataFormatada = formatador.format(now);
-
-  print("Data formatada: $dataFormatada" );
+  var mensagem = saudacoes("José Fulano");
+  print(mensagem);
 }
---
+++ introducao-dart/packages/my-simple-package/lib/src/my_simple_package_base.dart
@@ -0,0 +1,3 @@
+String saudacoes(String nome) {
+  return 'Olá, ' + nome;
+}
